863be6f7fc95d9817867795ef1e940c231ca5eba
Davide Ieria <davideieria99@gmail.com>
2023-11-28 18:00:21 +0100

    modifiche generali

--- src/pages/Preferiti.jsx
+++ src/pages/Preferiti.jsx
@@ -3,13 +3,6 @@ import { Context } from "../contexts/prefer";
 import PreferCard from "../components/PreferCard";
 
 export default function Preferiti() {
-    const { prefer } = useContext(Context)
-    const [data, setdata] = useState()
-    useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon`)
-            .then(r => r.json())
-            .then(r => setdata(() => r.results))
-    }, [])
 
 
     console.log(prefer)
